0143076e36d08cff5c70a9cb11cfb2eee59f3621
Doug Anderson444 <douganderson444@gmail.com>
2025-11-23 18:02:24 -0400

    straight up GViz

--- packages/web/src/bindgen.rs
+++ packages/web/src/bindgen.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
 use web_sys::{js_sys, Element};
 
 #[wasm_bindgen]
@@ -13,4 +14,29 @@ extern "C" {
 
     #[wasm_bindgen(method, js_name = renderSVGElement)]
     fn render_svg_element(this: &Viz, dot: &str) -> Element;
+}
+
+pub struct GViz {
+    instance: Viz,
+}
+
+#[derive(Debug)]
+pub enum VizError {
+    RenderError(String),
+}
+
+impl GViz {
+    async fn new() -> Result<Self, VizError> {
+        let promise = instance();
+        let js_instance = JsFuture::from(promise).await.map_err(|e| {
+            VizError::RenderError(format!("Failed to create Viz instance: {:?}", e))
+        })?;
+        let instance: Viz = js_instance.dyn_into().unwrap();
+        Ok(Self { instance })
+    }
+
+    fn render_dot(&self, dot: &str) -> String {
+        let element = self.instance.render_svg_element(dot);
+        element.outer_html()
+    }
 }
